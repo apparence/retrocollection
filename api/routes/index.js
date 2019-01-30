f9978df5f54add8801f4217cb928f684ed1c9796
Jordan Faucher <jordanfaucher1@gmail.com>
2019-01-30 14:04:18 +0100

    add randomstring

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,3 +7,18 @@ const router = require('express').Router();
 router.use('/subscribe', require('./subscribe'));
 
 module.exports = router;
+
+
+/*
+bcrypt.hash('myPassword', 10, function(err, hash) {
+  // Store hash in database
+});
+To verify the password later on:
+
+bcrypt.compare('somePassword', hash, function(err, res) {
+  if(res) {
+   // Passwords match
+  } else {
+   // Passwords don't match
+  }
+});*/
--- api/routes/subscribe.js
+++ api/routes/subscribe.js
@@ -1,16 +1,3 @@
-/*
-bcrypt.hash('myPassword', 10, function(err, hash) {
-  // Store hash in database
-});
-To verify the password later on:
-
-bcrypt.compare('somePassword', hash, function(err, res) {
-  if(res) {
-   // Passwords match
-  } else {
-   // Passwords don't match
-  }
-});*/
 
 const db = require('../data/config');
 const bodyParser = require('body-parser');
@@ -25,6 +12,7 @@ router.post('/', function(req, res, next) {
   } else if (!req.body.email) {
     res.status(400).send("Please put a mail");
   } else {
+    //ici rentrer dans a base les infos de l'utilisateur et le token et ensuite lui renvoyé le token
     res.status(200).send(req.body.email);
   }
 });
